a7172e894a7a247a5748155dd82fdf59a58aab8e
Adam Griffith <94396076+adamhgriffith-uofu@users.noreply.github.com>
2022-07-14 08:34:07 -0600

    Feature/20 investigate release version automation (#37)

--- scripts/semver-bump.py
+++ scripts/semver-bump.py
@@ -1,36 +1,47 @@
 #!/usr/bin/env python
 
 """
-This is a script to return an :func:`Exception` if a deployed semantic version is newer than
-its counterpart in the repository's ``Chart.yaml``, specifically the ``appVersion`` metadata.
+This is a script to return a GitHub output variable describing the bumped semantic version in the
+repository's ``Chart.yaml``, specifically the ``appVersion`` metadata.
 
-For simplicity this script uses system environmental variables as inputs including the following:
+This script uses the following system environmental variables as inputs:
 
 * ``RELEASE_VERSION``: derived from the repository's ``Chart.yaml``, specifically the ``appVersion``
   metadata (calculated by the GitHub workflow).
 * ``HELM_RELEASE_NAMESPACE``: specified as input to the GitHub workflow.
 * ``HELM_RELEASE_NAMESPACE_SHORTHAND``: derived from ``HELM_RELEASE_NAMESPACE`` by the GitHub workflow.
 * ``HELM_RELEASE_PREFIX``: specified as input to the GitHub workflow.
+* ``PRERELEASE_DATETIME_SUFFIX``: datetime object to apply as pre-release suffix
 
 Additionally, the tools installed and configured by the GitHub composite action
 ``gcloud-helm-setup/action.yml`` are required.
 """
 
-import os
 import logging
+import os
 import subprocess
 import semver
+import sys
 import yaml
+
+from datetime import datetime
 from yaml.loader import SafeLoader
 
-# Log all the things:
-logging.getLogger().setLevel(logging.DEBUG)
+# Set up logging:
+if 'DEBUG' in os.environ and os.environ['DEBUG'] == 'TRUE':
+    logging.getLogger().setLevel(logging.DEBUG)
+else:
+    logging.getLogger().setLevel(logging.INFO)
 
+# Main:
 if __name__ == '__main__':
-    discovered_appversion = os.environ.get('RELEASE_VERSION')
     helm_release_namespace = os.environ.get('HELM_RELEASE_NAMESPACE')
     helm_release_namespace_shorthand = os.environ.get('HELM_RELEASE_NAMESPACE_SHORTHAND')
     helm_release_prefix = os.environ.get('HELM_RELEASE_PREFIX')
+    prerelease_datetime_suffix = os.environ.get('PRERELEASE_DATETIME_SUFFIX')
+
+    if helm_release_namespace == 'production':
+        raise Exception("This script is not appropriate for the production environment.")
 
     command = [
         'helm', 'list',
@@ -49,25 +60,29 @@ if __name__ == '__main__':
                                 check=True,
                                 text=True
                                 ).stdout
-        logging.debug(f"Raw result from Helm: {result}")
+        logging.debug(f"Raw result from Helm:\n{result}")
         data = yaml.load(result, Loader=SafeLoader)
         deployed_appversion = data[0]['app_version']
         logging.info(f"Found deployed appVersion: {deployed_appversion}")
     except subprocess.CalledProcessError as ex:
         raise ex
 
-    logging.info("Discovering appVersion from source...")
-    logging.info(f"Found appVersion from source: {discovered_appversion}")
+    deployed_versioninfo = semver.VersionInfo.parse(deployed_appversion)
 
-    logging.info("Verifying newer appVersion...")
-    # deployed_appversion_parsed = semver.VersionInfo.parse(deployed_appversion)
-    # discovered_appversion_parsed = semver.VersionInfo.parse(discovered_appversion)
-    comparison = semver.compare(deployed_appversion, discovered_appversion)
-    logging.debug(f"Raw semver comparison result: {comparison}")
-    if semver.compare(discovered_appversion, deployed_appversion) < 1:
-        raise Exception(
-            f"The source appVersion \"{discovered_appversion}\" in Chart.yaml is not ahead" +
-            f" of the deployed appVersion \"{deployed_appversion}\". Update the source and" +
-            " try again."
-        )
-    logging.info("Source appVersion is correctly ahead of deployed appVersion.")
+    if helm_release_namespace == 'development':
+        new_versioninfo = deployed_versioninfo.finalize_version()
+        if semver.compare(str(new_versioninfo), str(deployed_appversion)) == 0:
+            new_versioninfo = deployed_versioninfo.finalize_version().bump_patch()
+
+        now = datetime.now()
+        date_time = now.strftime(prerelease_datetime_suffix)
+        app_version = f"{str(new_versioninfo)}-pre.{date_time}"
+
+        logging.info(f"New appVersion to apply: {app_version}")
+        sys.stdout.write(f"::set-output name=version::{app_version}\n")
+    else:
+        new_versioninfo = deployed_versioninfo.finalize_version().bump_patch()
+        app_version = f"{str(new_versioninfo)}"
+
+        logging.info(f"New appVersion to apply: {app_version}")
+        sys.stdout.write(f"::set-output name=version::{str(new_versioninfo)}\n")
